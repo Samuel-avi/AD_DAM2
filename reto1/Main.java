0b1d0585c1734dae1ba6bad069159fd1c37e30c4
Samuel <avilasanzsamuel@gmail.com>
2025-09-26 12:19:10 +0200

    Update Main.java

--- reto1/Main.java
+++ reto1/Main.java
@@ -3,6 +3,9 @@ package reto1;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 
@@ -41,20 +44,46 @@ public class Main {
 			
 			System.out.println("Valor la clave coche: " + p.getProperty("coche"));
 			
-			/* 3. no, porque solo se puede guardar un valor por clave, para eso habria que usar ObjectOutputStream o guardarlo en csv */
+			/// 3. no, porque solo se puede guardar un valor por clave, para eso habria que usar ObjectOutputStream o guardarlo en csv 
 			
 			
 		} catch (IOException e) {
-			System.out.println("Esta mal");
+			System.out.println("¿Que ha pasado?");
 		}
 		
 		
+		List<Coche> lc = new ArrayList<>();
 		
-		
-		
-		
-	
+		lc.add(new Coche("Toyota", "Corolla", "Rojo", 4));
+		lc.add(new Coche("Honda", "Civic", "Azul", 4));
+		lc.add(new Coche("Ford", "Focus", "Negro", 4));
+		lc.add(new Coche("Chevrolet", "Camaro", "Amarillo", 4));
+		lc.add(new Coche("Tesla", "Model 3", "Blanco", 4));
+		lc.add(new Coche("BMW", "X5", "Gris", 4));
+		lc.add(new Coche("Audi", "A4", "Plata", 4));
+		lc.add(new Coche("Mercedes", "C200", "Negro", 4));
+		lc.add(new Coche("Volkswagen", "Golf", "Verde", 4));
+		lc.add(new Coche("Fiat", "500", "Rojo", 4));
+		lc.add(new Coche("Jeep", "Wrangler", "Verde", 4));
+		lc.add(new Coche("Subaru", "Impreza", "Azul", 4));
+		lc.add(new Coche("Volvo", "XC90", "Plata", 4));
+		lc.add(new Coche("Porsche", "911", "Negro", 4));
 
+		try {
+			PrintWriter pw = new PrintWriter("coches.csv");
+			
+			pw.println("Marca,Modelo,Color,Ruedas");
+			
+			for (Coche coche : lc) {
+				pw.println(coche);
+			}
+			
+			pw.close();
+			
+		} catch (Exception e) {
+
+		} ///Siguiente ---> generar una nueva lista a partir del csv con BufferedReader 
+		
 	}
 
 }
